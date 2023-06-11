4433dd821996bb4285aef367c60501b535c07066
callmeweed <you@example.com>
2023-06-12 00:31:44 +0700

    add request and response

---
+++ backend/model/response.py
@@ -0,0 +1,17 @@
+from typing import Dict, List, Optional
+from pydantic import BaseModel
+from enum import Enum
+class Source(str,Enum):
+    batdongsanvn = "batdongsanvn" #https://batdongsan.vn/
+    nhatot = "nhatot" #https://www.nhatot.com/
+    houseviet = "houseviet" #https://houseviet.vn/
+    bds123vn = "bds123vn" #https://bds123.vn/
+
+class Response(BaseModel):
+    title:str
+    link:str
+    date:Optional[str] = None
+    thumbnail:Optional[str] = None #Link ảnh
+    area:Optional[str] = None   #diện tích
+    price:Optional[str] = None  #giá
+    source:Optional[Source] = None
