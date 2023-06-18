e8578322fb9e972beba55447c44c8f6b3592c81c
callmeweed <you@example.com>
2023-06-18 19:41:17 +0700

    add mubanservice

--- backend/model/response.py
+++ backend/model/response.py
@@ -7,6 +7,7 @@ class Source(str,Enum):
     houseviet = "houseviet" #https://houseviet.vn/
     bds123vn = "bds123vn" #https://bds123.vn/
     muabannet = "muabannet" #https://muaban.net/
+    batdongsancomvn = "batdongsan.com.vn"  # https://batdongsan.com.vn/
 
 class Response(BaseModel):
     title:str
